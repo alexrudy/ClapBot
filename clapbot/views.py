831bf0adccf71113172aaac8c82279a898bdced6
Alex Rudy <alex.rudy@gmail.com>
2017-04-22 15:53:36 -0700

    Fix sort order of listings

--- clapbot/views.py
+++ clapbot/views.py
@@ -6,26 +6,25 @@ from flask import render_template, send_file, redirect
 from .application import app, db
 from .model import Listing, Image
 
-from .notify import send_notification
+from .tasks import notify
 
 @app.route("/mail/")
 def mailer():
     """Mail things to me!"""
-    listings = Listing.query.order_by(Listing.created).filter_by(notified=False).limit(20).all()
-    send_notification(listings)
+    notify.delay()
     return redirect("/")
     
 
 @app.route("/latest/")
 def latest():
     """Render the latest few as if they were to be emailed."""
-    listings = Listing.query.order_by(Listing.created).limit(20)
+    listings = Listing.query.order_by(Listing.created.desc()).limit(20)
     return render_template("notify.html", listings=listings)
 
 @app.route("/")
 def home():
     """Homepage"""
-    listings = Listing.query.order_by(Listing.created)
+    listings = Listing.query.order_by(Listing.created.desc())
     return render_template("home.html", listings=listings)
 
 @app.route("/image/<int:identifier>/full.jpg")
