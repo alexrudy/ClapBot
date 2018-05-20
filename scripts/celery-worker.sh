0315715e03cba71936d973ff3e811bd0c01fd9b6
Alex Rudy <alex.rudy@gmail.com>
2018-05-19 19:43:32 -0700

    Add more concurrency to celery workers

--- scripts/celery-worker.sh
+++ scripts/celery-worker.sh
@@ -3,4 +3,4 @@ HOSTNAME=$(hostname)
 mkdir -p "./run/$HOSTNAME/"
 mkdir -p "./log/$HOSTNAME/"
 
-celery -A "${FLASK_APP:clapbot}:celery" worker -linfo
+celery -A "${FLASK_APP:clapbot}:celery" worker -linfo --concurrency=10
