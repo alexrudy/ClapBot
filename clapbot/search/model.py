341ecea83e686102fb9d3940ba34d8a26a8da887
Alex Rudy <alex.rudy@gmail.com>
2018-06-03 20:17:33 -0700

    Default filters for searches

--- clapbot/search/model.py
+++ clapbot/search/model.py
@@ -9,8 +9,7 @@ class HousingSearch(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
 
     owner_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    owner = db.relationship(
-        "User", backref=db.backref("housing_searches", uselist=True))
+    owner = db.relationship("User", backref=db.backref("housing_searches", uselist=True))
 
     created_at = db.Column(db.DateTime, default=dt.datetime.now)
     description = db.Column(db.String(255))
@@ -18,16 +17,26 @@ class HousingSearch(db.Model):
     target_date = db.Column(db.DateTime)
     expiration_date = db.Column(db.DateTime)
 
+    price_min = db.Column(db.Integer())
+    price_max = db.Column(db.Integer())
+
+    require_images = db.Column(db.Boolean)
+
+    @property
+    def filters(self):
+        data = {}
+        if self.price_max:
+            data['max_price'] = self.price_max
+        if self.price_min:
+            data['min_price'] = self.price_min
+        if self.require_images:
+            data['has_image'] = True
+        return data
+
     cl_site = db.Column(db.Integer(), db.ForeignKey('clsite.id'))
-    site = db.relationship(
-        "CraigslistSite",
-        backref=db.backref("searches", uselist=True, lazy='dynamic'))
+    site = db.relationship("CraigslistSite", backref=db.backref("searches", uselist=True, lazy='dynamic'))
     cl_area = db.Column(db.Integer(), db.ForeignKey('clarea.id'))
-    area = db.relationship(
-        "CraigslistArea",
-        backref=db.backref("searches", uselist=True, lazy='dynamic'))
+    area = db.relationship("CraigslistArea", backref=db.backref("searches", uselist=True, lazy='dynamic'))
 
     cl_category = db.Column(db.Integer(), db.ForeignKey('clcategory.id'))
-    category = db.relationship(
-        "CraigslistCategory",
-        backref=db.backref("searches", uselist=True, lazy='dynamic'))
+    category = db.relationship("CraigslistCategory", backref=db.backref("searches", uselist=True, lazy='dynamic'))
